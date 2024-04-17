e94c52c27b47389d64c953e2470dce83422b7f58
brycebien <brycebien@gmail.com>
2024-04-17 19:42:43 -0400

    can make passwords.enc.txt file

--- src/makepassword.js
+++ src/makepassword.js
@@ -3,11 +3,25 @@ const fs = require('fs');
 const {readFile, writeFile, hash} = require('./utility')
 
 function makepassword(passwordFileName, passwordEncFileName) {
-   ???
+    let data = readFile(passwordFileName);
+
+    let passwords = data.map(password => {
+        return hash(password.split(':')[1]);
+    });
+
+    let emails = data.map(email => {
+        return email.split(':')[0];
+    });
+
+    let combinedData = passwords.map((password, index) => {
+        return emails[index] + ':' + password;
+    });
+
+    writeFile(combinedData, passwordEncFileName)
 }
 
 if (require.main === module) {
     makepassword('./password.txt', './password.enc.txt')
 }
 
-module.exports = {???};
+module.exports = { makepassword };
